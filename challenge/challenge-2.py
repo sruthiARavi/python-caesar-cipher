2d8d8d6e43c272972abce2a3e8473b0389313fc3
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-09 16:02:54 -0400

    Update challenge-2.py

--- challenge/challenge-2.py
+++ challenge/challenge-2.py
@@ -12,21 +12,27 @@ shift = int(input("Type the shift number:\n"))
 def encrypt(original_text, shift_amount):
     encrypted_text = ""
     for letter in original_text:
-        original_index = alphabet.index(letter)
-        shifted_index = (original_index + shift_amount) % 26
-        encrypted_text += alphabet[shifted_index]
+        if letter == " ":
+            encrypted_text += letter
+        else:
+            original_index = alphabet.index(letter)
+            shifted_index = (original_index + shift_amount) % 26
+            encrypted_text += alphabet[shifted_index]
     print(f"Encrypted text: {encrypted_text}")
 
 def decrypt(encrypted_text, shift_amount):
     decrypted_text = ""
     for letter in encrypted_text:
-        shifted_index = alphabet.index(letter)
-        decrypted_index = shifted_index - shift_amount
-        if decrypted_index > 26:
-            decrypted_index %= 26
-        elif decrypted_index < 0:
-            decrypted_index += 26
-        decrypted_text += alphabet[decrypted_index]
+        if letter == " ":
+            decrypted_text += letter
+        else:
+            shifted_index = alphabet.index(letter)
+            decrypted_index = shifted_index - shift_amount
+            if decrypted_index > 26:
+                decrypted_index %= 26
+            elif decrypted_index < 0:
+                decrypted_index += 26
+            decrypted_text += alphabet[decrypted_index]
     print(f"Decrypted text: {decrypted_text}")
 
 if direction == "encode":
