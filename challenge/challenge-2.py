64e11e4288c8e3fce2b8941f02611a6a21cfbed1
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-08-09 17:02:12 -0400

    Update challenge-2.py

--- challenge/challenge-2.py
+++ challenge/challenge-2.py
@@ -9,6 +9,7 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
+#Separate encryption function
 def encrypt(original_text, shift_amount):
     encrypted_text = ""
     for letter in original_text:
@@ -16,10 +17,11 @@ def encrypt(original_text, shift_amount):
             encrypted_text += letter
         else:
             original_index = alphabet.index(letter)
-            shifted_index = (original_index + shift_amount) % 26
+            shifted_index = (original_index + shift_amount) % len(alphabet)
             encrypted_text += alphabet[shifted_index]
     print(f"Encrypted text: {encrypted_text}")
 
+#Separate decryption function
 def decrypt(encrypted_text, shift_amount):
     decrypted_text = ""
     for letter in encrypted_text:
@@ -27,15 +29,31 @@ def decrypt(encrypted_text, shift_amount):
             decrypted_text += letter
         else:
             shifted_index = alphabet.index(letter)
-            decrypted_index = shifted_index - shift_amount
-            if decrypted_index > 26:
-                decrypted_index %= 26
-            elif decrypted_index < 0:
-                decrypted_index += 26
+            decrypted_index = (shifted_index - shift_amount) % len(alphabet)
+            if decrypted_index < 0:
+                decrypted_index += len(alphabet)
             decrypted_text += alphabet[decrypted_index]
     print(f"Decrypted text: {decrypted_text}")
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+def caesar(encode_or_decode, text, shift):
+    output_text = ""
+    output_index = -1
+
+    # encode -> add shift, decode -> subtract shift
+    if encode_or_decode == "decode":
+        shift *= -1
+
+    for letter in text:
+        if letter == " ":
+            output_text += letter
+
+        else:
+            current_index = alphabet.index(letter)
+            output_index = (current_index + shift) % len(alphabet)
+            if output_index < 0:
+                output_index += len(alphabet)
+            output_text += alphabet[output_index]
+
+    print(f"Output text : {output_text}")
+
+caesar(direction, text, shift)
